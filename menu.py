9dd0875a9a974b4db561b2cedf8f39c8ac6d3e27
CSy-n <alpha.phonecian@gmail.com>
2020-08-11 20:29:07 +1000

    Updated menu.py to resolve file_path

--- menu.py
+++ menu.py
@@ -1,15 +1,11 @@
 from tkinter import *
 from tkinter.filedialog import askopenfilename
 
-
-def open_file_dialog():
-    path = askopenfilename()
-    return path
-
-
-
 def dialog_query_file():
     root = Tk()
-    b1 = Button(root, text='Open', command=open_file_dialog)
-    b1.pack()
-    root.mainloop()
+    root.withdraw()
+    root.update()
+    
+    file_path = askopenfilename()
+    root.destroy() 
+    return file_path

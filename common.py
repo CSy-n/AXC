8acda054de2b6f2bf02dd96a0b8c402c8f75205f
CSy-n <alpha.phonecian@gmail.com>
2020-10-03 18:01:13 +1000

    Further Updates. . . . . . . .

--- common.py
+++ common.py
@@ -3,7 +3,12 @@ from time import sleep
 from utility import *
 import os
 
+import system
+
 from menu import *
+
+MAX_CONFIRMATIONS = 5
+DIAGNOSTIC = False
 
 
 class Receiver(Thread):
@@ -14,26 +19,31 @@ class Receiver(Thread):
         self.socket = socket
 
     def run(self):
+        store_session_banner()
+
         try:
             while True:
-                s = self.socket.recv(2048)
-                if len(s) == 0:
-                    finish()
-                    return
-                message = s.decode()
+                data = self.socket.recv(2048)
+                if len(data) == 0:
+                    exit_application()
+
+                text = data.decode()
                 print("<<")
-                print(message)
+                print(text)
                 print(">>")
                 #If is server:
                 if self.server:
-                     # Check if (handle_command); check if "!file"
-                    handle_command(message)
-                    store_message(message)
+                    pass
 
+                     # Check if (handle_command); check if "!file"
+                store_message(text)
+                response = handle_command(text)
+
+                
                 
 
         except:
-            finish()
+            exit_application()
 
 class Sender(Thread):
     def __init__(self, socket, server=False):
@@ -42,22 +52,54 @@ class Sender(Thread):
         self.socket = socket
 
     def run(self):
-        try: 
-            while True:
-                print('>> ')
-                s = input()
+        while True:
 
-                sent = self.socket.send(s.encode())
-                if sent == 0:
-                    finish()
-                    return
-        except:
-            finish()
+            print('>> ')
+            text = ''
+
+            try:
+                text = input()
+            except KeyboardInterrupt:
+                exit_application()
+            except EOFError:  
+                confirm_exit_application()
+                print("Continuing...")
+
+            # text = self.socket.send(s.encode())
+            if DIAGNOSTIC:
+                print('~' + text)
+                print('len() => ' + str(len(text)))
+                
+            if text == ':quit':
+                exit_application()
+
+            sent = self.socket.send(text.encode())
+            if sent == 0:
+                print("SENT 0")
+                exit_application()
+                #     return
 
 
-def finish():
+def confirm_exit_application():
+  iteration = 0
+
+  while iteration < MAX_CONFIRMATIONS: 
+    try:
+        system.instrument()
+                
+        confirm = input('Would you like to Exit? [y\\n]: ')
+        if confirm.lower() in {"y", "yes"}:
+            exit_application()
+    except KeyboardInterrupt:
+        exit_application()
+    except EOFError:
+        print('')
+    iteration+=1
+
+def exit_application():
     print("Connection terminated")
     os._exit(0)
+
 
 def start_receiver_loop(socket, server=False):
     thread = Receiver(socket, server)
@@ -82,6 +124,9 @@ def handle_server(socket):
     receiver.join()
     send.join()
 
+"""Handles Commands:                           
+    - file :: opens a file-dialog               
+    - exec :: allows execution of arbitrary-code"""
 def handle_command(msg):
 
     # Check if Message is *actually* a command!
@@ -90,6 +135,8 @@ def handle_command(msg):
         file_path = dialog_query_file()
         print("[Opening File] ... ", end="")
         print(f"|{file_path}|")
+        return handle_file_dialog(file_path)
+
 
 class Server:
 
@@ -105,10 +152,10 @@ class Server:
 
                 sent = self.socket.send(s.encode())
                 if sent == 0:
-                    finish()
+                    exit_application()
                     return
         except:
-            finish()        
+            exit_application()        
     
 def print_server_diagnostics(server):
 
@@ -123,8 +170,11 @@ def print_server_diagnostics(server):
 
     
 
-def store_message(text):
+def store_session_banner():    
     path = "private/conversation.txt"
     storage_append_to_file(path, create_session_banner(0) )
+
+def store_message(text):
+    path = "private/conversation.txt"
     storage_append_to_file(path, text)
 
--- menu.py
+++ menu.py
@@ -1,11 +1,28 @@
-from tkinter import *
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askopenfilename as open_file_name
+from tkinter.filedialog import askdirectory as open_directory_name
+from tkinter import Tk
 
-def dialog_query_file():
+
+# Reference: https://pythonspot.com/tk-file-dialogs/
+
+def dialog_query_file(initial_directory="."):
     root = Tk()
     root.withdraw()
     root.update()
-    
-    file_path = askopenfilename()
+
+    file_path = open_file_name(initialdir=initial_directory,title= "Select file",
+                                   filetypes = (("All files","*.*"),("jpeg files","*.jpg")))
     root.destroy() 
     return file_path
+
+
+def dialog_query_directory():
+   root = Tk()
+   root.withdraw()
+   root.update()
+
+   file_path = open_directory_name()
+   root.destroy() 
+   return file_path
+
+                               
